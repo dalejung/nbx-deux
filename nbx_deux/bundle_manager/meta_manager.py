d6ad12b54709c4bc9a8876ea50fface0378e9035
Dale Jung <dale@dalejung.com>
2023-06-25 10:37:35 -0400

    oops wrong dir

--- nbx_deux/bundle_manager/meta_manager.py
+++
@@ -1,5 +0,0 @@
-from nbx_deux.bundle_manager.nbx_manager import NBXContentsManager
-
-
-class MetaManager(NBXContentsManager):
-    ...
---
+++ nbx_deux/meta_manager.py
@@ -0,0 +1,5 @@
+from nbx_deux.nbx_manager import NBXContentsManager
+
+
+class MetaManager(NBXContentsManager):
+    ...
